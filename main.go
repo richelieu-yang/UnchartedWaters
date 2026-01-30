96c59c45a9de1b891b66a8b41a1358025b9b28d0
Richelieu <richelieu042@gmail.com>
2026-01-30 15:20:25 +0800

    init

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/richelieu-yang/UnchartedWaters/src/adb"
+	"github.com/richelieu-yang/chimera/v3/src/log/console"
+)
+
 func main() {
+	a := &adb.Adb{
+		Address: "127.0.0.1:5555",
+	}
+	if err := a.CheckEnv(); err != nil {
+		console.Fatalf("a.CheckEnv() failed: %s", err)
+		return
+	}
 
 }
---
+++ src/adb/structs.go
@@ -0,0 +1,48 @@
+package adb
+
+import (
+	"context"
+
+	"github.com/richelieu-yang/chimera/v3/src/command/cmdKit"
+	"github.com/richelieu-yang/chimera/v3/src/core/errorKit"
+	"github.com/richelieu-yang/chimera/v3/src/log/console"
+)
+
+type Adb struct {
+	// Address 安卓设备的地址
+	/*
+		（1）基本语法：adb connect <设备IP地址>:<端口号>
+		（2）默认端口号是 5555，如果使用默认端口，可以省略端口号
+			e.g. adb connect 192.168.1.100 <=> adb connect 192.168.1.100:5555
+		（3）mac上，Bluestacks Air默认是 "127.0.0.1:5555"
+	*/
+	Address string
+}
+
+func (adb Adb) CheckEnv() error {
+	path, err := cmdKit.LookPath("adb")
+	if err != nil {
+		return errorKit.Wrapf(err, "fail to look path of adb")
+	}
+	console.Infof("adb path: %s", path)
+
+	// adb 版本号
+	{
+		str, err := cmdKit.RunCombinedlyToString(context.TODO(), "adb", "version")
+		if err != nil {
+			return errorKit.Wrapf(err, "fail to run 'adb version'")
+		}
+		console.Infof("adb version:\n%s", str)
+	}
+
+	// adb devices
+	{
+		str, err := cmdKit.RunCombinedlyToString(context.TODO(), "adb", "devices")
+		if err != nil {
+			return errorKit.Wrapf(err, "fail to run 'adb devices'")
+		}
+		console.Infof("adb devices:\n%s", str)
+	}
+
+	return nil
+}
